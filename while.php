2fe54cff1a67ff9397684310592125c441d3e49d
edire <az8641683@163.com>
2015-01-21 16:14:07 +0800

    这次我是真醉了

--- while.php
+++ while.php
@@ -7,7 +7,7 @@ ignore_user_abort();
 set_time_limit(0); 
 $interval=2; 
 //一直循环数据库里的内容 把 n 条记录取出 然后每个人的每门成绩进行对比
-do{
+//do{
 $rs = mysql_query($q, $dbh); 
 //下面这个循环是针对一个人的
 while( $row = mysql_fetch_array($rs) ){
@@ -28,18 +28,34 @@ while( $row = mysql_fetch_array($rs) ){
 		fclose($err_fp);
 		continue;
 	}
-	$arr_server  = $cur_arr["score"];
+	$arr_server = $cur_arr["score"];
+	for ($i = 0; $i < sizeof($arr_server); $i++) {
+		$kch = $arr_server[$i]['kch'];
+		$kc_server[$kch]['cj'] = $arr_server[$i]['cj'];
+		$kc_server[$kch]['kcm'] = $arr_server[$i]['kcm'];		
+
+		$kch = $arr_mysql[$i]['kch'];
+		$kc_mysql[$kch]['cj'] = $arr_mysql[$i]['cj'];
+		$kc_mysql[$kch]['kcm'] = $arr_mysql[$i]['kcm'];
+
+	}
+
+var_dump($kc_server);
+var_dump($kc_mysql);
+
 	for($i=0;$i<sizeof($arr_server);$i++){
-		
-		if($arr_mysql[$i]['cj'] == $arr_server[$i]['cj']){
-			$status = true;
-		}else{
-			$status = false;
-		}
+		$kch = $arr_server[$i]['kch'];
+	
+		if($kc_server[$kch]['cj'] == $kc_mysql[$kch]['cj']) {
+                       	$status = true;
+                }else{
+                       	$status = false;
+                }
 
 		//如果成绩改变 要干的事情，未来改成发送短信
 		if($status == false){
-			$message = "学科:".$arr_mysql[$i]['kcm']."由:".$arr_mysql[$i]['cj']." 更新到:" . $arr_server[$i]['cj'].'<br />';
+			$message = "学科:".$kc_mysql[$kch]['kcm']."由:".$kc_mysql[$kch]['cj']." 更新到:" . $kc_server[$kch]['cj'].'<br />';
+
 			$fp = fopen('sc.html','a');
 			fwrite($fp,$message);
 			fclose($fp);
@@ -47,11 +63,12 @@ while( $row = mysql_fetch_array($rs) ){
 	}
 	//转移范斜杠 存数据的时候防止被吃掉
 	$json_server = str_replace("\\", "\\\\", $json_server);
+
 	mysql_query("update user set cj_data = '".$json_server."' where id = '".$row['id']."'");
 	sleep($interval);
 }
 
 
-}while(true);
+//}while(true);
 
 ?>
