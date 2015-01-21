04c085927325952b6e1494bbaffc74b82badff43
edire <az8641683@163.com>
2015-01-21 19:54:02 +0800

    添加新用户功能 给新用户发提醒开通成功短信

--- add.php
+++ add.php
@@ -1,7 +1,17 @@
 <?
+	error_reporting(E_ALL^E_NOTICE);
 	$userid = $_GET['userid'];
 	$password = $_GET['password'];
+	$phone = $_GET['phone'];
 	$count = $_GET['count'];
+	if(!isset($userid) || !isset($password) || !isset($phone) || !isset($count)){		
+        	 $result = array(
+                       'error' => 201,
+                       'data' => 'data null'
+                 );
+                 echo json_encode($result);
+                 return;
+	}
 	$cj_data = file_get_contents('http://cj.ldustu.com/api/score?userid='.$userid.'&password='.$password.'&from=message&appid=10002&secret=message@ldsn');
 	$arr_data = json_decode($cj_data,true);
 	$error = $arr_data['error'];
@@ -13,6 +23,9 @@
 	$dbh = mysql_connect("127.0.0.1:3306","root","8641683");
 	if(!$dbh){die("error");}
 	mysql_select_db("cj", $dbh);
+
+
+
 	$sql = sprintf(
 		"select * from %s where user_id = %d",
 		mysql_real_escape_string('user'),
@@ -28,11 +41,33 @@
 		echo json_encode($result);
 		return;
 	}
+                $value = urlencode("#money#=".$count * 3 / 10);
+                $url='http://yunpian.com/v1/sms/tpl_send.json';
+                $post="apikey=0d679b8688ff199c06196328e578eeae&mobile=$phone&tpl_id=672601&tpl_value=$value";
+                $ch = curl_init($url);
+                curl_setopt($ch,CURLOPT_HEADER,0);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1) ;
+                curl_setopt($ch, CURLOPT_POST,1) ;
+                curl_setopt($ch, CURLOPT_POSTFIELDS,$post);
+                $message_result = curl_exec($ch);
+                curl_close($ch);
+                $json_message_result = json_decode($message_result,true);
+                $error = $json_message_result['code'];
+                if ($error != 0){
+                        $result = array(
+                                'error' => $error,
+                                'data' => $json_message_result['msg'],
+                                'detail' => $json_message_result['detail']
+                        );
+                        echo json_encode($result);
+                        return;
+                }
 	$sql = sprintf(
-		"insert into %s (user_id, pass, cj_data, count) values ('%s', '%s', '%s', %d)",
+		"insert into %s (user_id, pass, phone, cj_data, count) values ('%s', '%s', '%s', '%s', %d)",
 		mysql_real_escape_string('user'),
 		mysql_real_escape_string($userid),
 		mysql_real_escape_string($password),
+		mysql_real_escape_string($phone),
 		mysql_real_escape_string($cj_data),
 		mysql_real_escape_string($count)
 	);
@@ -41,5 +76,12 @@
 			'error' => 0,
 			'data' => 'success'
 		);
+		
 		echo json_encode($result);
+	}else{
+		$result = array(
+                        'error' => 203,
+                        'data' => 'sql error'
+                );
+                echo json_encode($result);
 	}
